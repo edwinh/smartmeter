1b7b2c36e3dd4ccee1826f894ba180f923b6c3c6
edwin_nl020 <ffsjettuh@hotmail.com>
2019-02-24 15:37:24 +0100

    dd

--- uitlezen.py
+++ uitlezen.py
@@ -33,7 +33,7 @@ except:
 p1_teller  = 0
 last_line = False
 
-while not last_line #p1_teller < 26:
+while not last_line: #p1_teller < 26:
     p1_line=''
     #Read 1 line van de seriele poort
     try:
@@ -45,7 +45,7 @@ while not last_line #p1_teller < 26:
     #if (p1_teller == 11 or p1_teller == 4):
     p1_str=str(p1_raw)
     p1_line=p1_str.strip()
-    if (p1_line[0] == '!')
+    if (p1_line[0] == '!'):
       last_line = True
     print ("Line " + str(p1_teller) + ": " + p1_line)
     p1_teller += 1
