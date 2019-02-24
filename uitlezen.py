1390cd844ae3c49f3cc2a0de9fd8a687bfbb0011
edwin_nl020 <ffsjettuh@hotmail.com>
2019-02-24 15:52:14 +0100

    dd

--- uitlezen.py
+++ uitlezen.py
@@ -31,7 +31,6 @@ except:
 #Initialize
 #p1_teller is mijn tellertje voor van 0 tot 36 te tellen
 p1_teller  = 0
-last_line = False
 
 while True: #not last_line: #p1_teller < 26:
     p1_line=''
@@ -42,12 +41,11 @@ while True: #not last_line: #p1_teller < 26:
         sys.exit ("Seriele poort %s kan niet gelezen worden. Aaaaaaaaarch." % ser.name )
     
     
-    #if (p1_teller == 11 or p1_teller == 4):
     p1_str=str(p1_raw)
     p1_line=p1_str.strip()
-    if (len(p1_line) > 0 and p1_line[0] == '!'):
-      last_line = True
-    print ("Line " + str(p1_teller) + ": " + p1_line)
+    if (p1_line.startswith("0-0:1.0.0")):
+      print("Datum/tijd: " + p1_line)
+    #print ("Line " + str(p1_teller) + ": " + p1_line)
     p1_teller += 1
 
 #Close port and show status
