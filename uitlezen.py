264a725ad3bf4a5cf9055e81193b45b75642cc09
edwin_nl020 <ffsjettuh@hotmail.com>
2019-03-03 12:41:21 +0100

    refactored

--- uitlezen.py
+++ uitlezen.py
@@ -6,6 +6,59 @@ import serial
 from datetime import datetime
 import extract_telegram
 
+def read_mock_data():
+  p1_teller = 0
+  while (p1_teller < len(extract_telegram.mock_data)):
+    p1_line=''
+    p1_line = extract_telegram.mock_data[p1_teller]
+    result = extract_telegram.decode_line(p1_line)
+    if result != None:
+      print (result)
+    
+    p1_teller += 1
+  return
+
+def read_live_data():
+  #Set COM port config
+  ser = serial.Serial()
+  ser.baudrate = 115200
+  ser.bytesize=serial.EIGHTBITS
+  ser.parity=serial.PARITY_NONE
+  ser.stopbits=serial.STOPBITS_ONE
+  ser.xonxoff=0
+  ser.rtscts=0
+  ser.timeout=20
+  ser.port="/dev/ttyUSB0"
+  telegram_length = 26
+
+  #Open COM port
+  try:
+      ser.open()
+  except:
+      sys.exit ("Fout bij het openen van %s. Aaaaarch."  % ser.name)
+
+  #Initialize
+  p1_teller = 0
+  done = False
+
+  while not done:
+      p1_line=''
+
+      #Read 1 line van de seriele poort
+      try:
+          p1_raw= ser.readline()
+          p1_str=str(p1_raw)
+          p1_line=p1_str.strip()
+      except:
+          sys.exit ("Seriele poort %s kan niet gelezen worden. Aaaaaaaaarch." % ser.name )
+
+      result = extract_telegram.decode_line(p1_line)
+      if result != None:
+        print (result)
+
+      done = (p1_line[0] == "!")
+  return
+
 def main():
   ##############################################################################
   #Main program
@@ -20,6 +73,13 @@ def main():
   else:
     mock = True
 
+  if mock:
+    read_mock_data()
+  else:
+    read_live_data()
+
+  return
+  
   if (not mock):
     #Set COM port config
     ser = serial.Serial()
