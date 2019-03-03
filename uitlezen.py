8baed398d3668b3dc2c25f84fea738c6778e5158
edwin_nl020 <ffsjettuh@hotmail.com>
2019-03-03 18:18:30 +0100

    dd

--- uitlezen.py
+++ uitlezen.py
@@ -46,13 +46,11 @@ def read_live_data():
       #Read 1 line van de seriele poort
       try:
           p1_raw= ser.readline()
-          print(p1_raw)
           p1_line = str(p1_raw).strip()
-          p1_line = p1_line[2:-3]
+          p1_line = p1_line[2:-4]
           print(p1_line)
       except:
           sys.exit ("Seriele poort %s kan niet gelezen worden. Aaaaaaaaarch." % ser.name )
-      continue
       first_line_read = first_line_read or (len(p1_line) > 0 and p1_line[0] == '/')
       if first_line_read:
         result = extract_telegram.decode_line(p1_line)
