1a762455c59bfd156c282a7f2caf78d775a0a174
edwin_nl020 <ffsjettuh@hotmail.com>
2019-03-03 18:19:48 +0100

    ss

--- uitlezen.py
+++ uitlezen.py
@@ -47,7 +47,8 @@ def read_live_data():
       try:
           p1_raw= ser.readline()
           p1_line = str(p1_raw).strip()
-          p1_line = p1_line[2:-4]
+          # Remove \r\n from the end of the lin
+          p1_line = p1_line[2:-5]
       except:
           sys.exit ("Seriele poort %s kan niet gelezen worden. Aaaaaaaaarch." % ser.name )
       first_line_read = first_line_read or (len(p1_line) > 0 and p1_line[0] == '/')
